741971b9526c06fd3939108e48b26c7c484c7982
XaviFast05 <xavicraft05@gmail.com>
2025-05-22 15:11:07 +0200

    alpha

---
+++ Juegos de desarrollo/ALPHA/Release/Assets/Maps/Navigation.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Navigation" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <image source="MapMetadata.png" width="140" height="140"/>
+</tileset>
